42a5c0ff7b26598c494e205bddb66ea21aa8743d
gabriel <lemento.tp@gmail.com>
2021-04-21 14:48:06 -0300

    variaveis, objetos, arrays, condições e repetição

---
+++ js01/functions.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+    // ==
+    // >=
+    // <=
+    // !=
+    // !==
+    // ===
+    var nome = "joaao",idade = '15', repetir=0;
+    if (nome == "joao"){
+        console.log('oi jão')
+    }else{
+        console.log('sai daqui seu lizo');
+    }
+    if (idade != 15){
+        console.log('carai você é vc vc vcvcvcvc')
+    }else{
+        console.log('pta merda ');
+    }
+    while (repetir < 1000){
+        console.log('Hello world!!');
+        repetir++;
+    }
+}
